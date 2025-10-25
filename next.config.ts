2d900f7ff3d64071655b0356fcf765e8c7dcf3d3
LuisGT84 <lateworks21@gmail.com>
2025-10-24 21:36:26 -0600

    feat: implementa búsqueda y paginación con URL params; optimiza con debounce; revierte cambios de PPR del cap.10

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    ppr: 'incremental', // Prerrenderización Parcial
-  }
 };
 
 export default nextConfig;
